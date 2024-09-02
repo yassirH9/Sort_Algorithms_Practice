3c835f1420bf48333c36eb54582fbcb6ad073de9
yassirH9 <yassirch2001ch@gmail.com>
2024-09-02 19:46:51 +0200

    added sort type bubble and insert

--- src/main/java/org/alg_practice/Main.java
+++ src/main/java/org/alg_practice/Main.java
@@ -3,6 +3,7 @@ package org.alg_practice;
 import org.alg_practice.algorithms.Sort;
 import org.alg_practice.utils.Helper;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +11,28 @@ public class Main {
     public static void main(String[] args) {
         Helper helper = new Helper();
         //Generate Random values list
-        List<Integer> randomArr = helper.genArr(10);
+        List<Integer> randomArr = helper.genArr(5);
         System.out.println(randomArr);
-        //Sorting
+
+
+        //SORT CODE
         Sort sort = new Sort();
-        List<Integer> sortedArr = sort.SelectionSort(randomArr);
+
+        //take the current time of the system to count execution time after sort
+        long startSortTime = System.nanoTime();
+        List<Integer> sortedArr = new ArrayList<>(); //list to place the new values sorted
+
+        //-> uncomment below to sort by selection
+        //sortedArr = sort.SelectionSort(randomArr);
+
+        //-> uncomment below to sort by bubble
+        //sortedArr = sort.BubbleSort(randomArr);
+
+        //-> uncomment below to sort by insertion
+        sortedArr = sort.InsertionSort(randomArr);
+        long endSortTime = System.nanoTime();
         System.out.println(sortedArr);
+        System.out.println("\033[0;32m"+"Sort process last: "+(endSortTime-startSortTime)+" ns | "+((endSortTime-startSortTime)/1_000_000)+" ms" +"\033[0m");
 
     }
 }
--- src/main/java/org/alg_practice/algorithms/Sort.java
+++ src/main/java/org/alg_practice/algorithms/Sort.java
@@ -6,6 +6,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Sort {
     public List<Integer> SelectionSort(List<Integer> inRandomArr){
+        //save the size of the list to operate the indexes of itself
         int size = inRandomArr.size();
 
         for(int i = 0; i<size; i++){
@@ -18,7 +19,8 @@ public class Sort {
                 }
             }
             if(!(minIndex == i)){
-                System.out.println("swap: current index ["+inRandomArr.get(i)+"] by minor found in index ["+inRandomArr.get(minIndex)+"]");
+                //-> uncomment the next, if you wanna se the procedure took sorting the list in the terminal.
+                //System.out.println("swap: current index ["+inRandomArr.get(i)+"] by minor found in index ["+inRandomArr.get(minIndex)+"]");
                 int temp = inRandomArr.get(minIndex);
                 inRandomArr.set(minIndex, inRandomArr.get(i));
                 inRandomArr.set(i,temp);
@@ -27,5 +29,41 @@ public class Sort {
         return inRandomArr;
     }
 
+    public List<Integer> BubbleSort(List<Integer> inRandomArr){
+        int size = inRandomArr.size();
+        for(int i=0; i< size-1; i++){
+            for(int a=0;a<size-1;a++){
+                if(inRandomArr.get(a)>inRandomArr.get(a+1)){
+                    //-> uncomment the next, if you wanna se the procedure took sorting the list in the terminal.
+                    //System.out.println("swap "+inRandomArr.get(a)+" for "+inRandomArr.get(a+1));
+                    //System.out.println(inRandomArr);
+                    int temp = inRandomArr.get(a+1);
+                    inRandomArr.set(a+1, inRandomArr.get(a));
+                    inRandomArr.set(a,temp);
+                }
+            }
+        }
 
+        return inRandomArr;
+    }
+
+    public List<Integer> InsertionSort(List<Integer> inRandomArr){
+        int size = inRandomArr.size();
+        for(int i=0; i<size;i++){
+            int key = inRandomArr.get(i); //value to sort
+            int val2 = i-1; //value to guide the sort process
+
+
+            while(val2>= 0 && inRandomArr.get(val2)>key){
+                inRandomArr.set(val2+1, inRandomArr.get(val2));
+                val2 = val2-1;
+            }
+
+            inRandomArr.set(val2+1, key);
+            //-> uncomment the next, if you wanna se the procedure took sorting the list in the terminal.
+            //System.out.println("key: "+key+" to position: "+(val2+1));
+            //System.out.println(inRandomArr);
+        }
+        return inRandomArr;
+    }
 }
